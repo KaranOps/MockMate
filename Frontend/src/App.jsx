cd0baec9a6af286657eb96efde0f8aba1b3b1337
Karankumar2004 <kk8594879@gmail.com>
2025-09-27 15:46:51 +0530

    Done Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-blue-500 bg-amber-300' >Hello, MockMate this side</h1>
+    </div>
+  )
+}
+
+export default App
